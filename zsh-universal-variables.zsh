f2d9217496906fb90466d99a1410cf295506d1ad
Jan Hoersch <hoja@tmt.de>
2012-12-12 11:47:39 +0100

    Trapping HUP Signal for cleanup

--- zsh-universal-variables.zsh
+++ zsh-universal-variables.zsh
@@ -13,17 +13,20 @@ function _omz_universalvariables_cleanupPTS() {
 	rm /tmp/zsh.$(whoami)/${PTS}
 }
 
+# usual exit with zsh hooks
 function _omz_universalvariables_zshexit() {
 	_omz_universalvariables_cleanupPTS
 }
 
-TRAPINT() {
-	_omz_universalvariables_cleanupPTS
-}
+# trapping signals sent to the shell
+TRAPINT() { _omz_universalvariables_cleanupPTS; }
+TRAPHUP() { _omz_universalvariables_cleanupPTS; } # just to be sure
 
 PTS="pts.${$(tty)##*/}"
 PID="pid.$$"
 
+# when a shell get's opened it sets all stuff it needs
+# and sets only permissions on the user
 mkdir -p /tmp/zsh.$(whoami)
 echo "" >| /tmp/zsh.$(whoami)/${PTS}
 chmod 700 -R /tmp/zsh.$(whoami)
